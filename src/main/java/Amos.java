b16c4ebaed5e27d36d1c150cd766b25500e15402
YSTey0808 <ystey0808@gmail.com>
2025-08-22 02:03:17 +0800

    Level-2

--- src/main/java/Amos.java
+++ src/main/java/Amos.java
@@ -1,9 +1,11 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Amos {
     public static final String  LINE = "\t------------------------------------------------------------";
     public static Scanner scan = new Scanner(System.in);
+    public static ArrayList<String> lst = new ArrayList<String>();
 
     public static void echo() {
         String res = scan.nextLine();
@@ -11,8 +13,25 @@ public class Amos {
         if(Objects.equals(res, "bye")){
             System.out.println("\tBye. Hope to see you again soon!");
             System.out.println(LINE);
+        } else if (Objects.equals(res, "")){
+            System.out.println("\tPlease say something!");
+            System.out.println(LINE);
+            echo();
+        } else if (Objects.equals(res, "list")){
+            if(lst.size() == 0) {
+                System.out.println("\t Nothing in the list now.");
+            } else {
+                for(int i = 0; i < lst.size(); i++){
+                    int j = i+1;
+                    System.out.println("\t " + j + ". " + lst.get(i));
+                }
+            }
+
+            System.out.println(LINE);
+            echo();
         } else {
-            System.out.println("\t" + res);
+            lst.add(res);
+            System.out.println("\t added: " + res);
             System.out.println(LINE);
             echo();
         }
